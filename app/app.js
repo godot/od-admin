a2c72108bbb13c6a448c0a33fd0fd6579dceccd1
dariusz gorzeba <dgorzeba@gmail.com>
2014-10-20 23:12:54 +0200

    cleanup and refactor

--- app/app.js
+++ app/app.js
@@ -1,12 +1,11 @@
 'use strict';
 
-angular.module('myApp', [
+angular.module('do', [
     'ngRoute',
-    'myApp.view1',
-    'myApp.view2',
-    'myApp.version',
+    'do.objects',
+    'do.records',
     'ngGrid'
 ]).
     config(['$routeProvider', function($routeProvider) {
-        $routeProvider.otherwise({redirectTo: '/view1'});
+        $routeProvider.otherwise({redirectTo: '/objects'});
     }]);
--- app/objects/objects.js
+++ app/objects/objects.js
@@ -1,18 +1,13 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
-    .factory('DcDatabase', function() {
-        var data = {};
-        return {
-            all: function() { return data; },
-            save: function(collection, object) {
-                var _object = angular.copy(object);
-                data[collection] = data[collection] || [];
-                data[collection].push(_object);
-            }
-        };
+angular.module('do.objects', ['ngRoute'])
 
-    })
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/objects', {
+            templateUrl: 'objects/objects.html'
+        });
+    }])
+
     .factory('DcObjectCollection', function() {
         var data = {
             role: { name: 'role', fields: [{name: 'name', type: 'text'}] },
@@ -42,67 +37,6 @@ angular.module('myApp.view1', ['ngRoute'])
             }
         };
     })
-
-    .directive('dcInput', ['$compile','$http','$templateCache','DcDatabase', function($compile,$http,$templateCache,db){
-        return {
-            restrict: 'E',
-            scope: {ngModel: '=', field: '=', type: '='},
-            link: function(scope , element, attrs) {
-                scope.$watch('field.collection', function(collection) {
-                    if (collection){
-                        scope.values = db.all()[collection];
-                    };
-                });
-
-                scope.$watch(attrs.type, function (type) {
-                    if (type) {
-                        loadTemplate('/fields/dc-'+type+'.html');
-                    }
-                });
-
-                //don't touch - it's magic ;)
-                function loadTemplate(template) {
-                    $http.get(template, { cache: $templateCache })
-                        .success(function(templateContent) {
-                            element.replaceWith($compile(templateContent)(scope));
-                        });
-                }
-            }
-        };
-    }])
-
-    .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/view1', {
-            templateUrl: 'view1/view1.html'
-        });
-    }])
-
-    .controller('RecordController', ['$scope','DcDatabase', function($scope,db) {
-        $scope.database = db;
-
-        $scope.save = function(collection, record){
-            db.save(collection,record);
-            $scope.add();
-            $scope.visible = false;
-        };
-        $scope.add = function(){
-            $scope.visible = true;
-        };
-        $scope.entry = {};
-    }])
-
-    .directive('dcRecordCollection', function(){
-        return {
-            restrict: 'A',
-            scope: true,
-            controller: 'RecordsController'
-        };
-    })
-
-    .controller('RecordsController', ['$scope','DcObjectCollection', function($scope,db) {
-        $scope.objects = db.all();
-    }])
-
 
     .directive('dcObjectForm', function(){
         return {
@@ -179,3 +113,5 @@ angular.module('myApp.view1', ['ngRoute'])
         };
 
     }]);
+
+;
